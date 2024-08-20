079851d05d428bbc3b9011860f11786d9d957490
Mercedes Egido <mercedes@huridocs.org>
2024-08-20 16:48:26 +0200

    Firebase store init - showing link url

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { firebaseConfig } from './firebaseconfig';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function getDocuments(db) {
+  const linkColection = collection(db, 'link');
+  const linkDocuments = await getDocs(linkColection);
+  const linksList = linkDocuments.docs.map(doc => doc.data());
+  return linksList;
+}
+
+const links = await getDocuments(db)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
+  
 
   return (
-    <>
+    <div style={{
+        display: "flex", 
+        flexDirection: "column", 
+        justifyContent: "center", 
+        alignItems: "center",
+        width: 1280
+      }}  >
       <h1>Hello World!</h1>
-      <div className="card">
+      {links.map((link) =>
+        <p key={link.id} style={{textAlign: "center"}}>{link.url}</p> 
+      )}
+      <div>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          There is {count} url for now 😎
         </button>
       </div>
-    </>
+    </div>
   )
 }
 
---
+++ src/firebaseconfig.js
@@ -0,0 +1,14 @@
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCgaJB8Jq04Nse3voLm8flrbaV7fQBljbk",
+  authDomain: "db-test-a2b82.firebaseapp.com",
+  databaseURL: "https://DATABASE_NAME.firebaseio.com",
+  projectId: "db-test-a2b82",
+  storageBucket: "db-test-a2b82.appspot.com",
+  messagingSenderId: "291215859077",
+  appId: "1:291215859077:web:a5978e46369859b205f8ec",
+  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+  measurementId: "G-MEASUREMENT_ID",
+};
+
