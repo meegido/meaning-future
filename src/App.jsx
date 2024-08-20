3fafb00a999f0beb6cc71b47622e7a6a0281c93f
Mercedes Egido <mercedes@huridocs.org>
2024-08-20 16:53:54 +0200

    removing inline style at html file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import { firebaseConfig } from './firebaseconfig';
