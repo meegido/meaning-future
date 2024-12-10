f5dd9ab753a30106dc159697b28f115643a96f53
Mercedes Egido <mercedes.egido@hey.com>
2024-12-10 19:09:21 +0100

    revert go back button

--- src/link/link-detail.tsx
+++ src/link/link-detail.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { getDocument } from '../shared/infrastructure/firestore-client';
 import { LinkInfo } from '../types';
 import styles from './link-detail.module.css';
@@ -7,7 +7,6 @@ import styles from './link-detail.module.css';
 export const LinkDetail = () => {
   const { id } = useParams();
   const [link, setLink] = useState<LinkInfo>({} as LinkInfo);
-  const navigate = useNavigate();
 
   useEffect(() => {
     (async () => {
@@ -22,7 +21,6 @@ export const LinkDetail = () => {
 
   return (
     <section className={styles.link__wrapper}>
-      <button onClick={() => navigate(-1)}>Go back</button>
       <h1>{link.title}</h1>
       <p>{link.perplexitySummary}</p>
     </section>
